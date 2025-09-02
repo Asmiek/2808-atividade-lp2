7a71ce3ae501149ab2c8322c5934a5299b45646e
Samuel AI <103371658+Asmiek@users.noreply.github.com>
2025-09-02 16:10:34 -0300

    Add files via upload

---
+++ aula laço repetição/issoai.js
@@ -0,0 +1,23 @@
+let banana = 0
+let nsei =100
+while (banana >= nsei){
+
+alert("rage")
+}
+let x = 10
+
+while(x >= 1){
+  console.log("Valor de x:", x);
+  x--;
+}
+console.log("laço do-while");
+x = 10
+do{
+  console.log("Valor de x:", x);
+  x--;
+} while(x >= 1);
+console.log("laço for");
+x = 10
+for(let i = x; i >= 1; i--){
+  console.log("Valor de i:", i);
+}
